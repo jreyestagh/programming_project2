4f1936bf691a24fc6224f99470e5ddbdff3fd4e7
Joshua Reyestan <jreyesta@stu.southtexascollege.edu>
2024-12-15 12:46:55 -0600

    Completed Timer Component

--- scripts/myScripts.js
+++ scripts/myScripts.js
@@ -12,5 +12,21 @@ function startCountdown() {
         alert("Please enter a time ranging between 1 and 60 seconds.");
         return;
     }
-    
+
+    startBtn.disabled = true;
+    timeInput.disabled = true;
+
+    let remainingTime = seconds;
+    const countdownTimer = setInterval(() => {
+        if (remainingTime > 0) {
+            console.log(`Time remaining: ${remainingTime} seconds`);
+            remainingTime--;
+        } else {
+            clearInterval(countdownTimer);
+            alert("Your time is up!!!!");
+            startBtn.disabled = false;
+            timeInput.disabled = false;
+            timeInput.value = "";
+        }
+    }, 1000);
 }
